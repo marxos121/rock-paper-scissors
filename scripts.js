388d1d59583419a69ff3b1b71358c0fc7dd0e772
marxos121 <marxos121@gmail.com>
2023-02-11 20:08:12 +0100

    add getComputerChoice function

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 10) % 3;
+    
+    if(choice === 0) { return 'Rock'; }
+    else if(choice === 1) { return 'Paper'; }
+    return "Scissors";
+}
