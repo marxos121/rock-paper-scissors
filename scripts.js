682c0849943e7aec221e00e54c88c4f45a99d889
marxos121 <marxos121@gmail.com>
2023-02-11 20:19:08 +0100

    add getPlayerChoice() function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 10) % 3;
     
-    if(choice === 0) { return 'Rock'; }
-    else if(choice === 1) { return 'Paper'; }
-    return "Scissors";
+    if(choice === 0) { return 'rock'; }
+    else if(choice === 1) { return 'paper'; }
+    return "scissors";
+}
+
+function getPlayerChoice()
+{
+    while(true){
+    let choice = prompt("Rock/Paper/Scissors?");
+    choice = choice.toLowerCase();
+
+        if(!(choice != 'rock' && choice != 'paper' && choice != 'scissors'))
+        {
+            return choice;
+        }
+    }
 }
