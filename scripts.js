9e31da1a697632a1730760ea7226cddfd2e41c35
marxos121 <marxos121@gmail.com>
2023-02-12 19:26:50 +0100

    update gameflow and add round results

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+function capitalize(text)
+{
+    return text.charAt(0).toUpperCase() + text.substr(1);
+}
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 10) % 3;
     
@@ -8,47 +13,68 @@ function getComputerChoice(){
 
 function getPlayerChoice()
 {
-    while(true){
-    let choice = prompt("Rock/Paper/Scissors?");
-    choice = choice.toLowerCase();
+    let choice = null;
+    while(!choice){
+        choice = prompt("Rock/Paper/Scissors?");
+        if(!choice)
+        {
+            continue;
+        }
 
-        if(!(choice != 'rock' && choice != 'paper' && choice != 'scissors'))
+        choice = choice.toLowerCase();
+
+        if(choice === 'rock' || choice === 'paper' || choice === 'scissors')
         {
             return choice;
         }
+
+        choice = null;
     }
 }
 
-function capitalize(text)
+function check(player, computer)
 {
-    return text.charAt(0).toUpperCase() + text.substr(1);
-}
-
-function playRound()
-{
-    let player = getPlayerChoice();
-    let computer = getComputerChoice();
-
     if((player === "rock" && computer === 'paper')
         || (player === "paper" && computer === 'scissors')
         || (player === "scissors" && computer === 'rock'))
     {
+        ++computerScore;
         return `You lose! ${capitalize(computer)} beats ${player}!`;
     } 
-
     else if((computer === "rock" && player === 'paper')
     || (computer === "paper" && player === 'scissors')
     || (computer === "scissors" && player === 'rock'))
     {
+        ++playerScore;
         return `You win! ${capitalize(player)} beats ${computer}!`;
     }
-
     else
     {
         return "Draw!";
     }
 }
 
-function game()
+function updateResult(roundResult)
 {
+    const res = document.querySelector('.round-result');
+    res.textContent = roundResult;
 }
+
+
+function playRound()
+{
+    let player = getPlayerChoice();
+    let computer = getComputerChoice();
+
+    let roundResult = check(player, computer);
+    updateResult(roundResult);
+}
+
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+buttons.forEach((button) => { 
+    button.addEventListener("click", playRound);
+});
+
+let playerScore = 0;
+let computerScore = 0;
